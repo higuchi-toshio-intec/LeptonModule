25f306f0bbf5d2e660f9df47969429d1f597eb97
Toshio HIGUCHI <higuchi_toshio@intec.co.jp>
2019-10-07 14:13:08 +0900

    This version is most simple. (1) Read frame data from SPI. (2) Print segment number.

--- software/raspberrypi_video/main.cpp
+++ software/raspberrypi_video/main.cpp
@@ -1,14 +1,3 @@
-#include <QApplication>
-#include <QThread>
-#include <QMutex>
-#include <QMessageBox>
-
-#include <QColor>
-#include <QLabel>
-#include <QtDebug>
-#include <QString>
-#include <QPushButton>
-
 #include "LeptonThread.h"
 #include "MyLabel.h"
 
@@ -98,32 +87,6 @@ int main( int argc, char **argv )
 		}
 	}
 
-	//create the app
-	QApplication a( argc, argv );
-	
-	QWidget *myWidget = new QWidget;
-	myWidget->setGeometry(400, 300, 340, 290);
-
-	//create an image placeholder for myLabel
-	//fill the top left corner with red, just bcuz
-	QImage myImage;
-	myImage = QImage(320, 240, QImage::Format_RGB888);
-	QRgb red = qRgb(255,0,0);
-	for(int i=0;i<80;i++) {
-		for(int j=0;j<60;j++) {
-			myImage.setPixel(i, j, red);
-		}
-	}
-
-	//create a label, and set it's image to the placeholder
-	MyLabel myLabel(myWidget);
-	myLabel.setGeometry(10, 10, 320, 240);
-	myLabel.setPixmap(QPixmap::fromImage(myImage));
-
-	//create a FFC button
-	QPushButton *button1 = new QPushButton("Perform FFC", myWidget);
-	button1->setGeometry(320/2-50, 290-35, 100, 30);
-
 	//create a thread to gather SPI data
 	//when the thread emits updateImage, the label should update its image accordingly
 	Lepton *myLepton = new Lepton();
@@ -134,16 +97,27 @@ int main( int argc, char **argv )
 	myLepton->setAutomaticScalingRange();
 	if (0 <= rangeMin) myLepton->useRangeMinValue(rangeMin);
 	if (0 <= rangeMax) myLepton->useRangeMaxValue(rangeMax);
-	LeptonThread *thread = new LeptonThread();
-	thread->setLepton(myLepton);
-	QObject::connect(thread, SIGNAL(updateImage(QImage)), &myLabel, SLOT(setImage(QImage)));
-	
-	//connect ffc button to the thread's ffc action
-	QObject::connect(button1, SIGNAL(clicked()), thread, SLOT(performFFC()));
-	thread->start();
-	
-	myWidget->show();
 
-	return a.exec();
+	LeptonAction *leptonAction = new LeptonAction();
+
+	//open Lepton port
+	myLepton->open();
+
+	int i = 0;
+	while(true) {
+		//read data packets from lepton over SPI
+		int segment_number = myLepton->readFrameData(leptonAction);
+		printf("%d", segment_number);
+
+		i++;
+		if ((i % 24)==0) {
+			printf("\n");
+		}
+	}
+
+	//finally, close Lepton just bcuz
+	myLepton->close();
+
+	return 0;
 }
 
