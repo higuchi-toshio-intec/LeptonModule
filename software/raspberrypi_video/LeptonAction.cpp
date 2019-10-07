c906aea9164721f89fd7d2e841b8890b0fd74082
Toshio HIGUCHI <higuchi_toshio@intec.co.jp>
2019-10-07 10:34:48 +0900

    Changed to reduce Qt dependencies

---
+++ software/raspberrypi_video/LeptonAction.cpp
@@ -0,0 +1,13 @@
+#include "LeptonAction.h"
+
+LeptonAction::LeptonAction()
+{
+}
+
+LeptonAction::~LeptonAction() {
+}
+
+void LeptonAction::setPixel(int col, int row, int colorR, int colorG, int colorB)
+{
+	// override this method
+}
---
+++ software/raspberrypi_video/LeptonAction.h
@@ -0,0 +1,17 @@
+#ifndef TEXTLEPTONACTION
+#define TEXTLEPTONACTION
+
+class LeptonAction
+{
+
+public:
+  LeptonAction();
+  ~LeptonAction();
+
+  virtual void setPixel(int, int, int, int, int);
+
+private:
+
+};
+
+#endif
